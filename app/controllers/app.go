8e1188dbd1644f1cb35eb062d8b6a34651c8ff5e
Chris Purta <cpurta@kochava.com>
2017-01-06 15:55:38 -0800

    Add blogs and get contact form to work

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,6 +1,11 @@
 package controllers
 
-import "github.com/revel/revel"
+import (
+	"crypto/tls"
+
+	"github.com/revel/revel"
+	gomail "gopkg.in/gomail.v2"
+)
 
 type App struct {
 	*revel.Controller
@@ -10,8 +15,12 @@ func (c App) Index() revel.Result {
 	return c.Render()
 }
 
-func (c App) Post(postID int) revel.Result {
-	return c.Render(postID)
+func (c App) Powerlifting() revel.Result {
+	return c.Render()
+}
+
+func (c App) Quines() revel.Result {
+	return c.Render()
 }
 
 func (c App) About() revel.Result {
@@ -26,6 +35,32 @@ func (c App) Encryption() revel.Result {
 	return c.Render()
 }
 
-func (c App) Contact() revel.Result {
+func (c App) Contact(name, email, phone, message string) revel.Result {
+	if c.Request.Method == "POST" {
+		c.Validation.Required(name).Message("Your name is required")
+		c.Validation.Required(email).Message("Your email is required")
+		c.Validation.MinSize(message, 20).Message("Your message must be at least 20 characters long.")
+
+		if c.Validation.HasErrors() {
+			c.Validation.Keep()
+			c.FlashParams()
+
+			return c.Render()
+		}
+
+		d := gomail.NewDialer("smtp.gmail.com", 587, "cpurta@gmail.com", "rwjavdofhsrkpxqg")
+		d.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+		m := gomail.NewMessage()
+		m.SetHeader("From", email)
+		m.SetHeader("To", "cpurta@gmail.com")
+		m.SetHeader("Subject", "New contact form from: "+name)
+		m.SetBody("text/html", message)
+
+		if err := d.DialAndSend(m); err != nil {
+			revel.ERROR.Println("Error sending email:", err.Error())
+		}
+	}
+
 	return c.Render()
 }
